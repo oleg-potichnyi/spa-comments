daf342018569c92b0fc1d1facea927c13b8a9a48
oleg-potichnyi <potichnyi.oleg@gmail.com>
2024-11-14 22:44:33 +0200

    Add Dockerfile, docker-compose.yml, and wait_for_db.py for Docker environment setup

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.10.6-slim-buster
+LABEL maintainer="potichnyi.oleg@gmail.com"
+
+ENV PYTHONUNBUFFERED=1
+
+WORKDIR /spa_comments
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+RUN mkdir -p /vol/web/media && \
+    chmod -R 755 /vol/web && \
+    chown -R root:root /vol/web
+
+EXPOSE 8000
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- spa_comments/settings.py
+++ spa_comments/settings.py
@@ -80,13 +80,13 @@ WSGI_APPLICATION = "spa_comments.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
-        "NAME": os.getenv("POSTGRES_DB", "app"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'app'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'secretpassword'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
